7734849a455aabe823f9a4fc586ea1b0be237d40
Vlad.Zherko <vladikon19@tut.by>
2025-01-17 00:06:58 +0300

    added: book-tracker-service dependencies

--- book-tracker-service/src/main/java/by/meshicage/BookTrackerServiceApplication.java
+++ book-tracker-service/src/main/java/by/meshicage/BookTrackerServiceApplication.java
@@ -1,13 +1,11 @@
-package by.meshicage.booktrackerservice;
+package by.meshicage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookTrackerServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BookTrackerServiceApplication.class, args);
     }
-
 }
