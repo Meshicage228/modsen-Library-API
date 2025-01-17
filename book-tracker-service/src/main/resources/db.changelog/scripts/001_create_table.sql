c803c8eaa4aaf949f7ddafdc1d0f3c7de3d1018b
Vlad.Zherko <vladikon19@tut.by>
2025-01-17 17:44:31 +0300

    changed: restructured table and given new constraints

--- book-tracker-service/src/main/resources/db.changelog/scripts/001_create_table.sql
+++ book-tracker-service/src/main/resources/db.changelog/scripts/001_create_table.sql
@@ -5,9 +5,9 @@
 CREATE TABLE IF NOT EXISTS books_tracking
 (
     id          SERIAL PRIMARY KEY,
-    book_id     INT     NOT NULL,
-    status      BOOLEAN NOT NULL,
-    borrowed_at TIMESTAMP,
-    return_by   TIMESTAMP,
-    FOREIGN KEY (book_id) REFERENCES books (id)
+    book_id     BIGINT  NOT NULL UNIQUE,
+    is_borrowed BOOLEAN NOT NULL DEFAULT FALSE,
+    borrowed_at TIMESTAMP DEFAULT NULL,
+    return_by   TIMESTAMP DEFAULT NULL,
+    is_deleted  BOOLEAN DEFAULT FALSE
 );
--- book-tracker-service/src/main/resources/db.changelog/scripts/002_fill_table_data.sql
+++ book-tracker-service/src/main/resources/db.changelog/scripts/002_fill_table_data.sql
@@ -2,14 +2,9 @@
 
 --changeset meshicage:2
 
-INSERT INTO books_tracking (book_id, status, borrowed_at, return_by)
+INSERT INTO books_tracking (book_id, is_borrowed, borrowed_at, return_by)
 VALUES (1, TRUE, '2025-01-01 10:00:00', '2025-01-07 10:00:00'),
        (2, TRUE, '2025-01-01 12:30:00', '2025-01-07 12:30:00'),
        (3, FALSE, NULL, NULL),
        (4, FALSE, NULL, NULL),
-       (5, FALSE, NULL, NULL),
-       (6, FALSE, NULL, NULL),
-       (7, FALSE, NULL, NULL),
-       (8, FALSE, NULL, NULL),
-       (9, FALSE, NULL, NULL),
-       (10, FALSE, NULL, NULL);
+       (5, FALSE, NULL, NULL);
