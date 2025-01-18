e28360b2c49f4e3d3a7cd9ae75a0ba39e55645c5
Vlad.Zherko <vladikon19@tut.by>
2025-01-18 18:30:19 +0300

    deleted and replaced to custom starter

--- authentication-service/src/main/java/by/meshicage/authenticationservice/exception/abstr/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package by.meshicage.authenticationservice.exception.abstr;
-
-public abstract class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- authentication-service/src/main/java/by/meshicage/authenticationservice/exception/user/IncorrectCredentialsException.java
+++
@@ -1,9 +0,0 @@
-package by.meshicage.authenticationservice.exception.user;
-
-public class IncorrectCredentialsException extends RuntimeException {
-    private static final String message = "Incorrect credentials provided";
-
-    public IncorrectCredentialsException() {
-        super(message);
-    }
-}
