404760cc381b9f4450c0452b40cdcc6b732a4fda
Vlad.Zherko <vladikon19@tut.by>
2025-01-19 00:16:17 +0300

    unit test init

--- book-tracker-service/src/main/java/by/meshicage/dto/UpdateBookTrackingStatus.java
+++ book-tracker-service/src/main/java/by/meshicage/dto/UpdateBookTrackingStatus.java
@@ -1,8 +1,10 @@
 package by.meshicage.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UpdateBookTrackingStatus {
     private Long bookId;
     private Boolean isBorrowed;
