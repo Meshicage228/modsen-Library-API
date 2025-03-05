2c3dcdce785e1c2825f85ad365cbd517794c5c53
Vlad.Zherko <vladikon19@tut.by>
2025-03-05 14:38:36 +0300

    updated: beautified localdatetime format

--- application-exception-starter/src/main/java/by/meshicage/applicationexceptionstarter/dto/ExceptionResponse.java
+++ application-exception-starter/src/main/java/by/meshicage/applicationexceptionstarter/dto/ExceptionResponse.java
@@ -1,5 +1,6 @@
 package by.meshicage.applicationexceptionstarter.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,5 +15,6 @@ import java.time.LocalDate;
 public class ExceptionResponse {
     private String message;
     private int status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDate timestamp;
 }
