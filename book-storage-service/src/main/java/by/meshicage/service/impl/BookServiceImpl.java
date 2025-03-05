4c9e4f1bc478e733d49a16ce0d464874d0a2c13c
Vlad.Zherko <vladikon19@tut.by>
2025-03-05 14:37:18 +0300

    added: cache integrated

--- book-storage-service/src/main/java/by/meshicage/service/impl/BookServiceImpl.java
+++ book-storage-service/src/main/java/by/meshicage/service/impl/BookServiceImpl.java
@@ -1,108 +1,56 @@
 package by.meshicage.service.impl;
 
-import by.meshicage.applicationexceptionstarter.exception.impl.book.BookNotFoundException;
-import by.meshicage.applicationexceptionstarter.exception.impl.book.BookUpdateException;
-import by.meshicage.applicationexceptionstarter.exception.impl.book.FailedToCreateBookException;
 import by.meshicage.dto.book.*;
-import by.meshicage.entity.BookEntity;
-import by.meshicage.entity.GenreEntity;
-import by.meshicage.kafka.KafkaProducer;
-import by.meshicage.mapper.BookMapper;
-import by.meshicage.repository.BookRepository;
 import by.meshicage.service.BookService;
-import by.meshicage.service.GenreService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
+@Service("bookService")
+@Primary
 public class BookServiceImpl implements BookService {
-    private final BookRepository bookRepository;
-    private final GenreService genreService;
-    private final KafkaProducer kafkaProducer;
-    private final BookMapper bookMapper;
+    private BookService cacheService;
 
-    @Override
-    @CachePut(value = "books", key = "#result.id")
-    public CreatedBookDto createBook(CreateBookDto createBookDto) {
-        return Optional.of(bookMapper.toBookEntity(createBookDto))
-                .map(bookEntity -> {
-                    GenreEntity byId = genreService.findById(createBookDto.getGenre().getId());
-                    bookEntity.setGenre(byId);
-                    return bookEntity;
-                })
-                .map(bookEntity -> {
-                    BookEntity saved = bookRepository.save(bookEntity);
-                    kafkaProducer.createBookTracking(saved.getId());
-                    return saved;
-                })
-                .map(bookMapper::toCreatedBookDto)
-                .orElseThrow(() -> new FailedToCreateBookException(createBookDto.getTitle()));
+    public BookServiceImpl(@Qualifier("bookCache") BookService cacheService) {
+        this.cacheService = cacheService;
     }
 
     @Override
-    @CachePut(value = "books", key = "#id")
+    public CreatedBookDto createBook(CreateBookDto createBookDto) {
+        return cacheService.createBook(createBookDto);
+    }
+
+    @Override
     @Transactional
     public UpdatedBookDto fullUpdate(Long id, FullBookUpdateDto fullBookUpdateDto) {
-        return bookRepository.findById(id)
-                .map(bookEntity -> {
-                    GenreEntity byId = genreService.findById(fullBookUpdateDto.getGenre().getId());
-                    bookEntity.setGenre(byId);
-                    return bookMapper.fullBookUpdate(bookEntity, fullBookUpdateDto);
-                })
-                .map(bookMapper::toUpdatedBookDto)
-                .orElseThrow(() -> new BookUpdateException(id));
+        return cacheService.fullUpdate(id, fullBookUpdateDto);
     }
 
-    // todo: isbn and id cache conflict
-//    @Caching(
-//            put = {@CachePut(value = "books", key = "#result.id"),
-//                    @CachePut(value = "books", key = "#result.isbn")
-//            })
     @Override
-    @CachePut(value = "books", key = "#id")
     @Transactional
     public UpdatedBookDto partUpdate(Long id, PartUpdateBookDto partUpdateBookDto) {
-        return bookRepository.findById(id)
-                .map(bookEntity -> bookMapper.partBookUpdate(bookEntity, partUpdateBookDto))
-                .map(bookMapper::toUpdatedBookDto)
-                .orElseThrow(() -> new BookUpdateException(id));
+        return cacheService.partUpdate(id, partUpdateBookDto);
     }
 
     @Override
-    @Cacheable(value = "books", key = "#id")
     public CreatedBookDto getBookById(Long id) {
-        return bookRepository.findById(id)
-                .map(bookMapper::toCreatedBookDto)
-                .orElseThrow(() -> new BookNotFoundException(id));
+        return cacheService.getBookById(id);
     }
 
     @Override
-    @Cacheable(value = "books", key = "#isbn")
     public CreatedBookDto getBookByISBN(String isbn) {
-        return bookRepository.findByIsbn(isbn)
-                .map(bookMapper::toCreatedBookDto)
-                .orElseThrow(() -> new BookNotFoundException(isbn));
+        return cacheService.getBookByISBN(isbn);
     }
 
     @Override
     public Page<CreatedBookDto> getAllBooks(Integer pageNum, Integer pageSize) {
-        return bookRepository.findAll(PageRequest.of(pageNum, pageSize))
-                .map(bookMapper::toCreatedBookDto);
+        return cacheService.getAllBooks(pageNum, pageSize);
     }
 
     @Override
-    @CacheEvict(value = "books", key = "#id")
     public void deleteBookById(Long id) {
-        bookRepository.deleteById(id);
-        kafkaProducer.deleteBookTracking(id);
+        cacheService.deleteBookById(id);
     }
 }
--- book-storage-service/src/main/java/by/meshicage/service/impl/GenreServiceImpl.java
+++ book-storage-service/src/main/java/by/meshicage/service/impl/GenreServiceImpl.java
@@ -1,20 +1,20 @@
 package by.meshicage.service.impl;
 
-import by.meshicage.applicationexceptionstarter.exception.impl.genre.GenreNotFoundException;
 import by.meshicage.entity.GenreEntity;
-import by.meshicage.repository.GenreRepository;
 import by.meshicage.service.GenreService;
+import by.meshicage.service.cache.GenreCacheService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 @RequiredArgsConstructor
 public class GenreServiceImpl implements GenreService {
-    private final GenreRepository genreRepository;
+    private final GenreCacheService genreCacheService;
 
     @Override
     public GenreEntity findById(Long id) {
-        return genreRepository.findById(id)
-                .orElseThrow(() -> new GenreNotFoundException(id));
+        return genreCacheService.findById(id);
     }
 }
